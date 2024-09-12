6581914ab16a730aa1d71e8f89ae062062eea3f1
kbventures <kmb_x@hotmail.com>
2024-09-12 11:17:45 -0500

    Modular types and extended thunk functionalityi for todo

--- src/features/todo/todoSlice.ts
+++ src/features/todo/todoSlice.ts
@@ -1,32 +1,13 @@
 // src/redux/slices/todosSlice.ts
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchTodos } from './todoThunk';
-// Define the type for a Todo item
-interface Todo {
-  id: number;
-  text: string;
-  completed: boolean;
-  createdAt: string;
-  updatedAt: string;
-}
-
-// Define the initial state type
-interface TodosState {
-  todos: Todo[];
-  status: 'idle' | 'loading' | 'failed';
-}
+import { TodosState } from '../../types/types'
 
 // Initial state
 const initialState: TodosState = {
   todos: [],
   status: 'idle',
 };
-
-// Async thunk to fetch todos from the server
-// export const fetchTodos = createAsyncThunk('todos/fetchTodos', async () => {
-//   const response = await axios.get<Todo[]>('http://localhost:8080/api/todos'); // Replace with your API endpoint
-//   return response.data;
-// });
 
 // Create the slice
 const todosSlice = createSlice({
--- src/features/todo/todoThunk.ts
+++ src/features/todo/todoThunk.ts
@@ -6,3 +6,18 @@ export const fetchTodos = createAsyncThunk('todos/fetchTodos', async () => {
     const response = await axios.get<Todo[]>('http://localhost:8080/api/todos'); // Replace with your API endpoint
     return response.data;
   });
+
+export const addTodo = createAsyncThunk('todos/addTodo', async () => {
+    const response = await axios.post<Todo[]>('http//localhost:8080/api/todos');
+    return response.data;
+});
+
+export const deleteTodo = createAsyncThunk('todos/deleteTodo', async (id:number) => {
+    const response = await axios.delete<Todo[]>(`http//localhost:8080/api/todos/${id}`);
+    return response.data;
+});
+
+export const toggleTodo = createAsyncThunk('todos/ToggleTodo', async (id:number) => {
+    const response = await axios.patch<Todo[]>(`http//localhost:8080/api/todos/${id}`);
+    return response.data;
+});
